95dccf445165f4191f5590516df3a8ad0cfe7de0
gggray000 <124888446+gggray000@users.noreply.github.com>
2025-04-08 20:27:51 +0200

    Fixed: dbc file loading, decode with base64

--- canbus/dbc_loader.py
+++ canbus/dbc_loader.py
@@ -1,4 +1,4 @@
 import cantools
 
 def load_dbc(file_path):
-    return cantools.database.load_file(file_path)
+    return cantools.db.load_file(file_path)
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from encodings.base64_codec import base64_decode
+import base64
+from base64 import b64decode
+
+# from encodings.base64_codec import base64_decode
 
 from mqtt.client import create_client
 from canbus.dbc_loader import load_dbc
@@ -9,6 +12,8 @@ import config
 
 dbc1 = load_dbc(config.DBC.get("file1"))
 dbc3 = load_dbc(config.DBC.get("file3"))
+print(f"DBC1 version:{dbc1.nodes}")
+print(f"DBC3 version:{dbc3.nodes}")
 writer = InfluxDBWriter()
 
 
@@ -19,10 +24,8 @@ def on_connect(client, userdata, flags, rc, properties):
         print("Connection failed")
     client.subscribe(config.MQTT.get("topic"))
 
-
-# TODO: Check dbccpp package, figure out why this is necessary in C++
 def on_message(client, userdata, msg):
-    data = base64_decode(msg.payload)
+    data = b64decode(msg.payload)
     print(f"Received message on topic {msg.topic}")
     print(f"Raw payload: {msg.payload}")
     for offset in range(0, len(data) - 17 + 1, 17):
@@ -31,24 +34,29 @@ def on_message(client, userdata, msg):
         dlc = data[offset + 4] & 0x0F
         can_data = data[offset + 5: offset + 5 + dlc]
         timestamp = struct.unpack(">i", data[offset + 13:offset + 17])[0]
+
         print(f"arbitration:{arbitration}")
         print(f"can_bus:{can_bus}")
         print(f"dlc:{dlc}")
         print(f"can_data:{can_data}")
         print(f"timestamp:{timestamp}")
+
+        if can_bus not in (0, 1):
+            print(f"Skipping unknown CAN bus {can_bus}")
+            continue
+
         if can_bus == 0:
             dbc = dbc1
         else:
             dbc = dbc3
 
-        # Verify this
-        msg_name, signals = dbc.decode_message(arbitration, can_data)
-        print(f"Message: {msg_name}({arbitration})")
-        print(f"Signals: {signals}")
-
-        if signals:
+        if arbitration in dbc._frame_id_to_message:
+            msg_name, signals = dbc.decode_message(arbitration, can_data)
+            print(f"Message: {msg_name}({arbitration})")
+            print(f"Signals: {signals}")
             writer.write(msg_name, f"canbus{can_bus}", signals, timestamp)
-
+        else:
+            print(f"Unknown arbitration ID: {arbitration}")
 
 client = create_client(on_connect, on_message)
 client.username_pw_set(config.MQTT.get("username"), config.MQTT.get("password"))
